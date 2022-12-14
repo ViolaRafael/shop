7d3f21c6964a99ef0a0c556438a69d78be3997b2
ViolaRafael <rafaelviola.eprosec@gmail.com>
2022-12-14 11:35:19 -0100

    Use Inherited Widget in practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shop/pages/counter_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
+import 'package:shop/providers/counter.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'pages/products_overview_page.dart';
 
@@ -11,21 +13,23 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.purple,
-        ).copyWith(
-          secondary: Colors.deepOrange,
+    return CounterProvider(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.purple,
+          ).copyWith(
+            secondary: Colors.deepOrange,
+          ),
         ),
+        home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (ctx) => CounterPage(),
+        },
+        debugShowCheckedModeBanner: false,
       ),
-      home: ProductsOverviewPage(),
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
-      },
-      debugShowCheckedModeBanner: false,
     );
   }
 }
