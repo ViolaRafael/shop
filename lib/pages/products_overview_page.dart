00a6f6eab3db3b1584d7a23dd43f22311a67098e
ViolaRafael <rafaelviola.eprosec@gmail.com>
2022-12-15 14:11:33 -0100

    Integrate Cart Provider

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/badge.dart';
 import 'package:shop/components/product_grid.dart';
+import 'package:shop/models/cart.dart';
 
 enum FilterOptions {
   Favorite,
@@ -14,7 +17,6 @@ class ProductsOverviewPage extends StatefulWidget {
 }
 
 class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
-
   bool _showFavoriteOnly = false;
   @override
   Widget build(BuildContext context) {
@@ -36,14 +38,24 @@ class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
             ],
             onSelected: (FilterOptions selectedValue) {
               setState(() {
-              if(selectedValue == FilterOptions.Favorite) {
-                _showFavoriteOnly = true;
-              }else {
-                _showFavoriteOnly = false;
-              }
+                if (selectedValue == FilterOptions.Favorite) {
+                  _showFavoriteOnly = true;
+                } else {
+                  _showFavoriteOnly = false;
+                }
               });
             },
           ),
+          Consumer<Cart>(
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.shopping_cart),
+            ),
+            builder: (ctx, cart, child) => Badge(
+              value: cart.itemsCount.toString(),
+              child: child!,
+            ),
+          )
         ],
       ),
       body: ProductGrid(_showFavoriteOnly),
