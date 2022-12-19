8b6b9b609f731ee2ef09d2f150f7c9006e4b02a8
ViolaRafael <rafaelviola.eprosec@gmail.com>
2022-12-19 16:48:43 -0100

    Create Product Management Widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/orders_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
+import 'package:shop/pages/products_page.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'models/cart.dart';
 import 'pages/products_overview_page.dart';
@@ -46,6 +47,7 @@ class MyApp extends StatelessWidget {
           AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
           AppRoutes.CART: (ctx) => CartPage(),
           AppRoutes.ORDERS: (ctx) => OrdersPage(),
+          AppRoutes.PRODUCTS: (ctx) => ProductsPage(),
         },
         debugShowCheckedModeBanner: false,
       ),
