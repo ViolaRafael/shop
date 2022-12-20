bd74b9a15a48938d8a17ee6fcc0c0291c53f71c3
ViolaRafael <rafaelviola.eprosec@gmail.com>
2022-12-20 15:37:27 -0100

    Add Option to remove Products

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
 import '../models/product.dart';
 
@@ -28,7 +30,31 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).colorScheme.primary,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                showDialog<bool>(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: const Text('Excluir Produto'),
+                    content: const Text('Tem certeza?'),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.of(ctx).pop(),
+                        child: const Text('Não'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(ctx).pop(false);
+                          Provider.of<ProductList>(
+                            context,
+                            listen: false,
+                          ).removeProduct(product);
+                        },
+                        child: const Text('Sim'),
+                      ),
+                    ],
+                  ),
+                );
+              },
               icon: const Icon(Icons.delete),
               color: Theme.of(context).colorScheme.error,
             ),
