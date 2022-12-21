46e5e223849e66433eb0d6b66cb6d0ffdb8f8286
ViolaRafael <rafaelviola.eprosec@gmail.com>
2022-12-21 16:48:43 -0100

    Show Products from database

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -2,13 +2,12 @@ import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
   final _url =
       'https://shop-a9cbf-default-rtdb.europe-west1.firebasedatabase.app/products.json';
-  final List<Product> _items = dummyProducts;
+  final List<Product> _items = [];
 
   List<Product> get items => [..._items];
   List<Product> get favoriteItems =>
@@ -20,7 +19,21 @@ class ProductList with ChangeNotifier {
 
   Future<void> loadProducts() async {
     final response = await http.get(Uri.parse(_url));
-    print(jsonDecode(response.body));
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((productId, productData) {
+      _items.add(
+        Product(
+          id: productId,
+          name: productData['name'],
+          description: productData['description'],
+          price: productData['price'],
+          imageUrl: productData['imageUrl'],
+          isFavorite: productData['isFavorite'],
+        ),
+      );
+    });
+    notifyListeners();
   }
 
   /// checks to see if product submitted(_submitForm()) already had an id and sends product information
