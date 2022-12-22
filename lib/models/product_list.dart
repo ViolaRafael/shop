218ae4533c4861e5d04fecb2e8b676226222663a
ViolaRafael <rafaelviola.eprosec@gmail.com>
2022-12-22 09:37:53 -0100

    Fix Update Product

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -5,8 +5,8 @@ import 'package:http/http.dart' as http;
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
-  final _url =
-      'https://shop-a9cbf-default-rtdb.europe-west1.firebasedatabase.app/products.json';
+  final _baseUrl =
+      'https://shop-a9cbf-default-rtdb.europe-west1.firebasedatabase.app/products';
   final List<Product> _items = [];
 
   List<Product> get items => [..._items];
@@ -19,7 +19,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> loadProducts() async {
     _items.clear();
-    final response = await http.get(Uri.parse(_url));
+    final response = await http.get(Uri.parse('$_baseUrl.json'));
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((productId, productData) {
@@ -61,7 +61,7 @@ class ProductList with ChangeNotifier {
   // async requires the method to be Future
   Future<void> addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse('$_baseUrl.json'),
       body: jsonEncode(
         {
           "name": product.name,
@@ -86,14 +86,23 @@ class ProductList with ChangeNotifier {
 
   /// using product's information from saveProduct it changes the product's details using the id to identify
   /// it's index
-  Future<void> updateProduct(Product product) {
+  Future<void> updateProduct(Product product) async {
     int index = _items.indexWhere((p) => p.id == product.id);
     if (index >= 0) {
+      await http.patch(
+        Uri.parse('$_baseUrl/${product.id}.json'),
+        body: jsonEncode(
+          {
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "imageUrl": product.imageUrl,
+          },
+        ),
+      );
       _items[index] = product;
       notifyListeners();
     }
-
-    return Future.value();
   }
 
   /// when user presses the yes button when trying to delete a product this function is called
