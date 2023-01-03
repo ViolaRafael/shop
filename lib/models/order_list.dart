fb42835dbbf07ec5ee042216f7d179790c4f90c3
ViolaRafael <rafaelviola.eprosec@gmail.com>
2023-01-03 17:00:42 -0100

    Add Orders based on user

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -8,10 +8,12 @@ import 'order.dart';
 
 class OrderList with ChangeNotifier {
   final String _token;
+  final String _userId;
   List<Order> _items = [];
 
   OrderList([
     this._token = '',
+    this._userId = '',
     this._items = const [],
   ]);
 
@@ -27,7 +29,7 @@ class OrderList with ChangeNotifier {
   Future<void> loadOrders() async {
     List<Order> items = [];
     final response = await http
-        .get(Uri.parse('${Constants.ORDER_BASE_URL}.json?auth=$_token'));
+        .get(Uri.parse('${Constants.ORDER_BASE_URL}/$_userId.json?auth=$_token'));
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((orderId, orderData) {
@@ -57,7 +59,7 @@ class OrderList with ChangeNotifier {
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
     final response = await http.post(
-      Uri.parse('${Constants.ORDER_BASE_URL}.json?auth=$_token'),
+      Uri.parse('${Constants.ORDER_BASE_URL}/$_userId.json?auth=$_token'),
       body: jsonEncode(
         {
           'total': cart.totalAmount,
